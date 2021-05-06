c7fe12ce1d237db47fab29505a25f41da92b2231
Bashar Ahmed <bashar_a@cs.iitr.ac.in>
2021-05-06 17:05:11 +0530

    hover feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -331,6 +331,37 @@ canvas.addEventListener("click",function(event){
         endg=true;
     }
 });
+canvas.addEventListener("mousemove",function(event){
+    var mx=event.clientX;
+    var my=event.clientY;
+    var cx=1100;
+    var cy=300;
+    var dist=Math.sqrt(((mx-cx)*(mx-cx))+((my-cy)*(my-cy)));
+    if(dist<200&&lbpg){
+        var shift=100;
+        tool.clearRect(900,100,200,200);
+        tool.fillStyle = 'red';
+        tool.beginPath();
+        tool.arc(1000+shift,300,200,0,Math.PI*2);
+        tool.fill();
+        tool.closePath();
+        tool.fillStyle = 'white';
+        tool.font = "60px Arial";
+        tool.fillText("Play Again",860+shift,320);
+    }
+    else{
+        var shift=100;
+        tool.clearRect(900,100,200,200);
+        tool.fillStyle = 'chartreuse';
+        tool.beginPath();
+        tool.arc(1000+shift,300,200,0,Math.PI*2);
+        tool.fill();
+        tool.closePath();
+        tool.fillStyle = 'white';
+        tool.font = "60px Arial";
+        tool.fillText("Play Again",860+shift,320);
+    }
+});
 generator();
 var interval=requestAnimationFrame(move);
 // var interval=setInterval(move,2);
